8d753e8bb876742a7ea3efd81243bad2af5850df
hak <hak@hak-PC>
2015-05-08 22:24:01 +0900

    콜백 인터페이스 제너릭적용하여 리턴타입 다양화

--- SpringTest30/src/main/java/springbook/learningtest/template/Calculator.java
+++ SpringTest30/src/main/java/springbook/learningtest/template/Calculator.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Calculator {
 	public Integer calcSum(String filepath) throws IOException{
-		LineCallback sumcallback = new LineCallback() {
+		LineCallback<Integer> sumcallback = new LineCallback<Integer>() {
 			public Integer doSomethingWithLine(String line, Integer value) {
 				return value + Integer.valueOf(line);
 			}
@@ -15,7 +15,7 @@ public class Calculator {
 	}
 	
 	public Integer calcMultiply(String filepath) throws IOException{
-		LineCallback muliplycallback = new LineCallback() {
+		LineCallback<Integer> muliplycallback = new LineCallback<Integer>() {
 			public Integer doSomethingWithLine(String line, Integer value) {
 				return value * Integer.valueOf(line);
 			}
@@ -23,34 +23,20 @@ public class Calculator {
 		return lineReadTemplate(filepath, muliplycallback, 1);
 	}
 	
-	public Integer fileReadTemplate(String filepath, BufferedReaderCallBack callback) throws IOException{
-		BufferedReader br = null;
-		
-		try {
-			br = new BufferedReader(new FileReader(filepath)); // 한 줄씩 일기 편하게 BufferedReader로 파일을 가져온다.
-			int ret = callback.doSomethingWithReader(br);
-			
-			return ret;
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			throw e;
-		} finally {
-			if(br != null){	// BufferedReader 오브젝트가 생성되기 전에 예외가 발생할 수도 있기 때문에 null 체크 
-				try {
-					br.close();
-				} catch (Exception e) {
-					System.out.println(e.getMessage());
-				}
+	public String concatenate(String filepath) throws IOException{
+		LineCallback<String> concatenateCallback = new LineCallback<String>() {
+			public String doSomethingWithLine(String line, String value) {
+				return value + line;
 			}
-			
-		}
+		};
+		return lineReadTemplate(filepath, concatenateCallback, "");
 	}
 	
-	public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException{
+	public <T>T lineReadTemplate(String filepath, LineCallback<T> callback, T initVal) throws IOException{
 		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(filepath)); // 한 줄씩 일기 편하게 BufferedReader로 파일을 가져온다.
-			Integer res = initVal;
+			T res = initVal;
 			String line = null;
 			
 			while((line = br.readLine()) != null){
