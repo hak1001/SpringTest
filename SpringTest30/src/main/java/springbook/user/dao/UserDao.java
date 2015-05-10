a41602c0c338b777b991fdd2f60db3695c11a2c2
hak <hak@hak-PC>
2015-05-10 16:01:34 +0900

    add(), deleteAll() jdbcTemplate.update() 적용 getCount() jdbcTemplate.query() 적용

--- SpringTest30/src/main/java/springbook/user/dao/UserDao.java
+++ SpringTest30/src/main/java/springbook/user/dao/UserDao.java
@@ -5,37 +5,31 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.naming.spi.DirStateFactory.Result;
 import javax.sql.DataSource;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 import springbook.user.domain.User;
 
 	public class UserDao {
 	
 	private DataSource dataSource;
-	private JdbcContext jdbcContext;
+	private JdbcTemplate jdbcTemplate;
 	
 	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 		this.dataSource = dataSource;
-		this.jdbcContext = new JdbcContext();
-		this.jdbcContext.setDataSource(dataSource);
+		
 	}
 	
 	public void add(final User user) throws ClassNotFoundException, SQLException{
-		this.jdbcContext.workWithStatementStrategy(
-			new StatementStrategy() {
-				public PreparedStatement makePreparedStatement(Connection c) throws SQLException{
-					PreparedStatement ps = c.prepareStatement(
-								"insert into users(id, name, password) values(?,?,?)");
-					ps.setString(1,  user.getId());
-					ps.setString(2,  user.getName());
-					ps.setString(3,  user.getPassword());
-					
-					return ps;
-				}
-			}
-		);
+		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)"
+				,user.getId(), user.getName(), user.getPassword());
 	}
 	
 	public User get(String id) throws ClassNotFoundException, SQLException{
@@ -66,28 +60,21 @@ import springbook.user.domain.User;
 	}
 	
 	public void deleteAll() throws SQLException {
-		this.jdbcContext.executeSql("delete from users");
+		this.jdbcTemplate.update("delete from users");
 	}
 	
 	public int getCount() throws SQLException{
-		
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		
-		try{
-			c = dataSource.getConnection();
-			ps = c.prepareStatement("select count(*) from users");
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e){
-			throw e;
-		} finally {
-			if (rs != null) { try { rs.close(); } catch (SQLException e) {} }
-			if (ps != null) { try { ps.close(); } catch (SQLException e) {} }
-			if (c != null) { try {c.close(); } catch (SQLException e) {} }
-		}
+		return this.jdbcTemplate.query(new PreparedStatementCreator() {
+			public PreparedStatement createPreparedStatement(Connection con)
+					throws SQLException {
+				return con.prepareStatement("select count(*) from users");
+			}
+		}, new ResultSetExtractor<Integer>() {
+			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException{
+				rs.next();
+				return rs.getInt(1);
+			}
+		});
 	}
 	
 }
