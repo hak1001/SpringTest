378bfad17190299b6dcdc6dffd29efb4b2f78e79
hak <hak@hak-PC>
2015-05-17 19:15:45 +0900

    UserService 추가

---
+++ SpringTest30/src/main/java/springbook/user/service/UserServiceTest.java
@@ -0,0 +1,61 @@
+package springbook.user.service;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.junit.matchers.JUnitMatchers.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import springbook.user.dao.UserDao;
+import springbook.user.domain.Level;
+import springbook.user.domain.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/test-applicationContext.xml")
+public class UserServiceTest {
+	@Autowired
+	UserService userService;
+	@Autowired UserDao userDao;
+	
+	List<User> users;
+	
+	@Before
+	public void setUp(){
+		// Arrays.asList 배열을 리스트로 만들어주는 메소드
+		users = Arrays.asList(
+			new User("admin", "관리자", "p1", Level.BASIC, 49, 0),
+			new User("master", "마스터", "p2", Level.BASIC, 50, 0),
+			new User("suser1", "사용자1", "p3", Level.SILVER, 60, 29),
+			new User("tuser2", "사용자2", "p4", Level.SILVER, 60, 30),
+			new User("wuser3", "사용자3", "p5", Level.GOLD, 100, 100)
+		);
+	}
+	
+	@Test
+	public void upgradeLevels(){
+		userDao.deleteAll();
+		for(User user : users) userDao.add(user);
+		
+		userService.upgradeLevels();
+		
+		checkLevel(users.get(0), Level.BASIC);
+		checkLevel(users.get(1), Level.SILVER);
+		checkLevel(users.get(2), Level.SILVER);
+		checkLevel(users.get(3), Level.GOLD);
+		checkLevel(users.get(4), Level.GOLD);
+	}
+	
+	private void checkLevel(User user, Level expectedLeve){
+		User userUpdate = userDao.get(user.getId());
+		assertThat(userUpdate.getLevel(), is(expectedLeve));
+				
+	}
+}
