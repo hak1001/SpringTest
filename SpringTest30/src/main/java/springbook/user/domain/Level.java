c271b86e4a756d650a2aa3d7ec5c43a1956aa51f
hak <hak@hak-PC>
2015-05-17 21:40:19 +0900

    enum 변경 - DB에 저장할 값과, 다음단계 레벨 추가. 다음단계 레벨 nextLevel() 메소드 추가

--- SpringTest30/src/main/java/springbook/user/domain/Level.java
+++ SpringTest30/src/main/java/springbook/user/domain/Level.java
@@ -1,16 +1,23 @@
 package springbook.user.domain;
 
 public enum Level {
-	BASIC(1), SILVER(2), GOLD(3);
+	// 이늄 선언에 DB에 저장할 값과 함께 다음 단계의 레벨정보 추가
+	GOLD(3, null), SILVER(2, GOLD), BASIC(1, SILVER);
 	
 	private final int value;
+	private final Level next;
 	
-	Level(int value){
+	Level(int value, Level next){
 		this.value = value;
+		this.next = next;
 	}
 	
 	public int intValue(){
 		return value;
+	}
+	
+	public Level nextLevel(){
+		return this.next;
 	}
 	
 	public static Level valueOf(int value){
