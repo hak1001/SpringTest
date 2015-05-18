ba4796c5d51f39d24034eb93248e9d61de5abc0d
hak <hak@hak-PC>
2015-05-18 21:33:57 +0900

    upgradeLevels 트랜잭션 적용

--- SpringTest30/src/main/java/springbook/user/service/UserService.java
+++ SpringTest30/src/main/java/springbook/user/service/UserService.java
@@ -1,30 +1,60 @@
 package springbook.user.service;
 
+import java.sql.Connection;
 import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 public class UserService {
-	UserDao userDao;
-	
 	public static final int MIN_LOGCOUNT_FOR_SILVER = 50;
 	public static final int MIN_RECOMMEND_FOR_GOLD = 30;
+
+	private UserDao userDao;
 	
 	public void setUserDao(UserDao userDao){
 		this.userDao = userDao;
 	}
 	
+	private DataSource dataSource;
+	
+	public void setDataSource(DataSource dataSource){
+		this.dataSource = dataSource;
+	}
+	
 	// 레벨 업그레이드 메소드
-	public void upgradeLevels(){
-		List<User> users = userDao.getAll();
-		for(User user : users){
-			// 업그레이드 가능 확인용 메소드와 업그레이드 작업 메소드로 리팩토링
-			if(canUpgradeLevel(user)){
-				upgradeLevel(user);
+	public void upgradeLevels() throws Exception{
+		// 트랜잭션 동기화 관리자를 이용해 동기화 작업을 초기화 한다.
+		TransactionSynchronizationManager.initSynchronization();
+		Connection c = DataSourceUtils.getConnection(dataSource);
+		c.setAutoCommit(false);
+		
+		try {
+			List<User> users = userDao.getAll();
+			for(User user : users){
+				// 업그레이드 가능 확인용 메소드와 업그레이드 작업 메소드로 리팩토링
+				if(canUpgradeLevel(user)){
+					upgradeLevel(user);
+				}
 			}
+			c.commit();
+		} catch (Exception e) {
+			c.rollback();
+			throw e;
+		} finally {
+			// 스프링 유틸리티 메소드를 이용해 DB 커넥션을 안전하게 닫는다. 
+			DataSourceUtils.releaseConnection(c, dataSource);
+			// 동기화 작업 종료 및 정리
+			TransactionSynchronizationManager.unbindResource(this.dataSource);
+			TransactionSynchronizationManager.clearSynchronization();
 		}
+		
 	}
 	
 	// 레벨 업그레이드 가능 확인 메소드
@@ -40,7 +70,7 @@ public class UserService {
 	}
 	
 	// 레벨 업그레이드 작업용 메소드
-	private void upgradeLevel(User user){
+	protected void upgradeLevel(User user){
 		user.upgradeLevel();
 		userDao.update(user);
 	}
