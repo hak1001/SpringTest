0dcac13b20771ba4b072bddd0ca293c589f2d927
hak <hak@hak-PC>
2015-05-19 22:33:14 +0900

    로그수정

--- SpringTest30/src/main/java/springbook/user/service/DummyMailSender.java
+++ SpringTest30/src/main/java/springbook/user/service/DummyMailSender.java
@@ -6,12 +6,12 @@ import org.springframework.mail.SimpleMailMessage;
 
 public class DummyMailSender implements MailSender{
 	public void send(SimpleMailMessage mailMessage) throws MailException{
-		System.out.println("=================== 메일발송 테스트 시작 ===================");
+		System.out.println("=================== DummyMailSender 메일발송 테스트 시작 ===================");
 		System.out.println("from === " + mailMessage.getFrom());
-		System.out.println("to === " + mailMessage.getTo());
+		System.out.println("to === " + mailMessage.getTo()[0]);
 		System.out.println("subject === " + mailMessage.getSubject());
 		System.out.println("text === " + mailMessage.getText());
-		System.out.println("=================== 메일발송 테스트 완료 ===================");
+		System.out.println("=================== DummyMailSender 메일발송 테스트 완료 ===================\n");
 	}
 	
 	public void send(SimpleMailMessage[] mailMessage) throws MailException{
