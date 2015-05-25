32caaa48a858639490383f585f3f38291d365a91
hak <hak@hak-PC>
2015-05-25 16:48:24 +0900

    다이내믹 프록시 테스트 추가

---
+++ SpringTest30/src/main/java/springbook/learningtest/jdk/proxy/DynamicProxyTest.java
@@ -0,0 +1,89 @@
+package springbook.learningtest.jdk.proxy;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.junit.Test;
+
+public class DynamicProxyTest {
+	@Test
+	public void simpleProxy(){
+		Hello hello = new HelloTarget();
+		assertThat(hello.sayHello("hak1001"), is("Hello hak1001"));
+		assertThat(hello.sayHi("hak1001"), is("Hi hak1001"));
+		assertThat(hello.sayThankYou("hak1001"), is("ThankYou hak1001"));
+		
+		// 프록시 테스트
+		//Hello proxiedHello = new HelloUppercase(new HelloTarget());
+		Hello proxiedHello = (Hello)Proxy.newProxyInstance(	// 생성된 다이내믹 프록시 오브젝트는 Hello 인터페이스를 구현하고 있으므로 Hello 타입으로 캐스팅해도 안전..
+				getClass().getClassLoader(),	// 동적으로 생성되는 다이내믹 프록시 클래스의 로딩에 사용할 클래스 로더
+				new Class[] { Hello.class },	// 구현할 인터페이스. 한번에 하나 이상의 인터페이스를 구현할 수도 있기 때문에 인터페이스 배열 사용
+				new UppercaseHandler(new HelloTarget()));	// 부가기능과 위임 코드를 담은 InvocationHandler
+		
+		assertThat(proxiedHello.sayHello("hak1001"), is("HELLO HAK1001"));
+		assertThat(proxiedHello.sayHi("hak1001"), is("HI HAK1001"));
+		assertThat(proxiedHello.sayThankYou("hak1001"), is("THANKYOU HAK1001"));
+	}
+	
+	static class HelloUppercase implements Hello{
+		Hello hello; // 위임할 타깃 오브젝트. 인터페이스로 접근
+		
+		public HelloUppercase(Hello hello){
+			this.hello = hello;
+		}
+		
+		public String sayHello(String name){
+			return hello.sayHello(name).toUpperCase(); // 위임과 부가기능 적용
+		}
+		
+		public String sayHi(String name){
+			return hello.sayHi(name).toUpperCase();
+		}
+		
+		public String sayThankYou(String name){
+			return hello.sayThankYou(name).toUpperCase();
+		}
+		
+	}
+	
+	// InvocationHandler 구현 클래스
+	static class UppercaseHandler implements InvocationHandler{
+		Hello target;
+		
+		public UppercaseHandler(Hello target){
+			this.target = target;
+		}
+		
+		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable{
+			String ret = (String)method.invoke(target, args);	// 타깃으로 위임, 인터페이스의 메소드 호출에 모두 적용됨.
+			return ret.toUpperCase();	// 부가기능 제공
+		}
+	}
+	
+	// Hello 인터페이스
+	static interface Hello{
+		String sayHello(String name);
+		String sayHi(String name);
+		String sayThankYou(String name);
+	}
+	
+	// 타깃 클래스
+	static class HelloTarget implements Hello{
+		public String sayHello(String name){
+			return "Hello " + name; 
+		}
+		
+		public String sayHi(String name){
+			return "Hi " + name; 
+		}
+		
+		public String sayThankYou(String name){
+			return "ThankYou " + name; 
+		}
+	}
+	
+}
