39c5e0a235535c2694ccfebc6d1d617217e89ae1
hak <hak@hak-PC>
2015-05-25 21:32:43 +0900

    테스트 메소드 upgradeAllOrNothing() 에 다이내맥 프록시 적용

--- SpringTest30/src/main/java/springbook/user/service/UserServiceTest.java
+++ SpringTest30/src/main/java/springbook/user/service/UserServiceTest.java
@@ -11,6 +11,7 @@ import static org.mockito.Mockito.when;
 import static springbook.user.service.UserServiceImpl.MIN_LOGCOUNT_FOR_SILVER;
 import static springbook.user.service.UserServiceImpl.MIN_RECOMMEND_FOR_GOLD;
 
+import java.lang.reflect.Proxy;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -152,9 +153,16 @@ public class UserServiceTest {
 		testUserService.setUserDao(this.userDao);
 		testUserService.setMailSender(mailSender);
 		
-		UserServiceTx txUserService = new UserServiceTx();
-		txUserService.setTransactionManager(transactionManager);
-		txUserService.setUserService(testUserService);
+		TransactionHandler txHandler = new TransactionHandler();
+		//	트랜잭션 핸들러가 필요한 정보와 오브젝트 DI
+		txHandler.setTarget(testUserService);
+		txHandler.setTransactionManager(transactionManager);
+		txHandler.setPattern("upgradeLevels");
+		//	UserService 인터페이스 타입의 다이내맥 프록시 생성
+		UserService txUserService = (UserService)Proxy.newProxyInstance(
+				getClass().getClassLoader(), 
+				new Class[] { UserService.class }, 
+				txHandler);
 		
 		userDao.deleteAll();
 		for(User user : users) userDao.add(user);
