3936c7b58abe072b93ca1e9c54b2d2e119c0bbab
hak <hak@hak-PC>
2015-06-01 22:22:55 +0900

    자바 코드를 이용한 빈 설정 클래스 추가

---
+++ SpringTest30/src/main/java/springbook/TestApplicationContext.java
@@ -0,0 +1,10 @@
+package springbook;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("/test-applicationContext.xml")
+public class TestApplicationContext {
+
+}
