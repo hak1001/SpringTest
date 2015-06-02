791078f1e1912fe700bc449c3d6fdefaa0e4dcfc
hak <hak@hak-PC>
2015-06-02 21:44:45 +0900

    메소드로 변환한 빈 정의

--- SpringTest30/src/main/java/springbook/TestApplicationContext.java
+++ SpringTest30/src/main/java/springbook/TestApplicationContext.java
@@ -1,11 +1,30 @@
 package springbook;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.mail.MailSender;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import springbook.user.dao.UserDao;
+import springbook.user.dao.UserDaoJdbc;
+import springbook.user.service.DummyMailSender;
+import springbook.user.service.UserService;
+import springbook.user.service.UserServiceImpl;
+import springbook.user.service.UserServiceTest.TestUserService;
+import springbook.user.sqlservice.OxmSqlService;
+import springbook.user.sqlservice.SqlRegistry;
+import springbook.user.sqlservice.SqlService;
+import springbook.user.sqlservice.updatable.EmbeddedDbSqlRegistry;
 
 import com.mysql.jdbc.Driver;
 
@@ -23,4 +42,47 @@ public class TestApplicationContext {
 		
 		return dataSource;
 	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager(){
+		DataSourceTransactionManager tm = new DataSourceTransactionManager();
+		tm.setDataSource(dataSource());
+		return tm;
+	}
+	
+	/**
+	 * 애플리케이션 로직 & 테스트용 빈
+	 */
+	
+	@Autowired SqlService sqlService;
+	
+	@Bean
+	public UserDao userDao(){
+		UserDaoJdbc dao = new UserDaoJdbc();
+		dao.setDataSource(dataSource());
+		dao.setSqlService(this.sqlService);
+		return dao;
+	}
+	
+	@Bean
+	public UserService userService(){
+		UserServiceImpl service = new UserServiceImpl();
+		service.setUserDao(userDao());
+		service.setMailSender(mailSender());
+		return service;
+	}
+	
+	@Bean
+	public UserService testUserService(){
+		TestUserService testService = new TestUserService();
+		testService.setUserDao(userDao());
+		testService.setMailSender(mailSender());
+		return testService;
+	}
+	
+	@Bean
+	public MailSender mailSender(){
+		return new DummyMailSender();
+	}
+	
 }
