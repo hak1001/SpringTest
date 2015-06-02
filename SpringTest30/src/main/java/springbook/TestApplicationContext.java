1791284c9f06b0586d33ce1e0972dad2212e0307
hak <hak@hak-PC>
2015-06-02 23:04:17 +0900

    @ComonentScan 애노테이션 적용 후 UserDao, UserService 메소드 제거

--- SpringTest30/src/main/java/springbook/TestApplicationContext.java
+++ SpringTest30/src/main/java/springbook/TestApplicationContext.java
@@ -2,16 +2,14 @@ package springbook;
 
 import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.HSQL;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.mail.MailSender;
 import org.springframework.oxm.Unmarshaller;
@@ -20,10 +18,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import springbook.user.dao.UserDao;
-import springbook.user.dao.UserDaoJdbc;
 import springbook.user.service.DummyMailSender;
 import springbook.user.service.UserService;
-import springbook.user.service.UserServiceImpl;
 import springbook.user.service.UserServiceTest.TestUserService;
 import springbook.user.sqlservice.OxmSqlService;
 import springbook.user.sqlservice.SqlRegistry;
@@ -34,6 +30,7 @@ import com.mysql.jdbc.Driver;
 
 @Configuration
 @EnableTransactionManagement	// <tx:annotation-driven /> 을 대신하는 애노테이션
+@ComponentScan(basePackages="springbook.user")
 public class TestApplicationContext {
 	@Bean
 	public DataSource dataSource(){
@@ -59,28 +56,12 @@ public class TestApplicationContext {
 	 * 애플리케이션 로직 & 테스트용 빈
 	 */
 	
-	@Autowired SqlService sqlService;
-	
-	@Bean
-	public UserDao userDao(){
-		UserDaoJdbc dao = new UserDaoJdbc();
-		dao.setDataSource(dataSource());
-		dao.setSqlService(this.sqlService);
-		return dao;
-	}
-	
-	@Bean
-	public UserService userService(){
-		UserServiceImpl service = new UserServiceImpl();
-		service.setUserDao(userDao());
-		service.setMailSender(mailSender());
-		return service;
-	}
+	@Autowired UserDao userDao;
 	
 	@Bean
 	public UserService testUserService(){
 		TestUserService testService = new TestUserService();
-		testService.setUserDao(userDao());
+		testService.setUserDao(this.userDao);
 		testService.setMailSender(mailSender());
 		return testService;
 	}
