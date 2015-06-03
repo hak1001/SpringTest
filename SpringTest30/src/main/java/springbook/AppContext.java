3c2b0dba19bf39fe4d96230a10c368e1424d38d3
hak <hak@hak-PC>
2015-06-03 23:05:03 +0900

    테스트 DI TestAppContext.class로 분리

--- SpringTest30/src/main/java/springbook/AppContext.java
+++ SpringTest30/src/main/java/springbook/AppContext.java
@@ -4,23 +4,17 @@ import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.
 
 import javax.sql.DataSource;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.mail.MailSender;
 import org.springframework.oxm.Unmarshaller;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import springbook.user.dao.UserDao;
-import springbook.user.service.DummyMailSender;
-import springbook.user.service.UserService;
-import springbook.user.service.UserServiceTest.TestUserService;
 import springbook.user.sqlservice.OxmSqlService;
 import springbook.user.sqlservice.SqlRegistry;
 import springbook.user.sqlservice.SqlService;
@@ -49,26 +43,6 @@ public class AppContext {
 		DataSourceTransactionManager tm = new DataSourceTransactionManager();
 		tm.setDataSource(dataSource());
 		return tm;
-	}
-	
-	
-	/**
-	 * 애플리케이션 로직 & 테스트용 빈
-	 */
-	
-	@Autowired UserDao userDao;
-	
-	@Bean
-	public UserService testUserService(){
-		TestUserService testService = new TestUserService();
-		testService.setUserDao(this.userDao);
-		testService.setMailSender(mailSender());
-		return testService;
-	}
-	
-	@Bean
-	public MailSender mailSender(){
-		return new DummyMailSender();
 	}
 	
 	
