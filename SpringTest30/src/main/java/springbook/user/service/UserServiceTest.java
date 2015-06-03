b0e6eedc41e4012b3cd705acf72ee7ee2366d3db
hak <hak@hak-PC>
2015-06-03 23:03:49 +0900

    테스트 컨텍스트의 DI 설정 클래스 정보 수정

--- SpringTest30/src/main/java/springbook/user/service/UserServiceTest.java
+++ SpringTest30/src/main/java/springbook/user/service/UserServiceTest.java
@@ -32,13 +32,14 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import springbook.TestApplicationContext;
+import springbook.AppContext;
+import springbook.TestAppContext;
 import springbook.user.dao.UserDao;
 import springbook.user.domain.Level;
 import springbook.user.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=TestApplicationContext.class)
+@ContextConfiguration(classes={AppContext.class, TestAppContext.class})
 public class UserServiceTest {
 	@Autowired ApplicationContext context;
 	@Autowired UserService userService;
