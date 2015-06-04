97ea4b99e7377807b55d2add9bfc80967c853b91
hak <hak@hak-PC>
2015-06-04 21:41:07 +0900

    @Profile 애노테이션 적용

--- SpringTest30/src/main/java/springbook/TestAppContext.java
+++ SpringTest30/src/main/java/springbook/TestAppContext.java
@@ -1,16 +1,16 @@
 package springbook;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.MailSender;
 
-import springbook.user.dao.UserDao;
 import springbook.user.service.DummyMailSender;
 import springbook.user.service.UserService;
 import springbook.user.service.UserServiceTest.TestUserService;
 
 @Configuration
+@Profile("test")
 public class TestAppContext {
 	/**
 	 * 애플리케이션 로직 & 테스트용 빈
