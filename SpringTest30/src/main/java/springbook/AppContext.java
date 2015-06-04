702495e5ec3f2da2bc09444c7c8747a475e59f7b
hak <hak@hak-PC>
2015-06-04 22:05:38 +0900

    중첩 클래스를 이용하여 프로파일 적용

--- SpringTest30/src/main/java/springbook/AppContext.java
+++ SpringTest30/src/main/java/springbook/AppContext.java
@@ -6,17 +6,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import springbook.user.service.DummyMailSender;
+import springbook.user.service.UserService;
+import springbook.user.service.UserServiceTest.TestUserService;
 
 import com.mysql.jdbc.Driver;
 
 @Configuration
 @EnableTransactionManagement	// <tx:annotation-driven /> 을 대신하는 애노테이션
 @ComponentScan(basePackages="springbook.user")
-@Import({SqlServiceContext.class, TestAppContext.class, ProductionAppContext.class})
+@Import(SqlServiceContext.class)
 public class AppContext {
 	@Bean
 	public DataSource dataSource(){
@@ -35,5 +42,34 @@ public class AppContext {
 		DataSourceTransactionManager tm = new DataSourceTransactionManager();
 		tm.setDataSource(dataSource());
 		return tm;
+	}
+	
+	@Configuration
+	@Profile("test")
+	public static class TestAppContext {
+		/**
+		 * 애플리케이션 로직 & 테스트용 빈
+		 */
+		
+		@Bean
+		public UserService testUserService(){
+			return new TestUserService();
+		}
+		
+		@Bean
+		public MailSender mailSender(){
+			return new DummyMailSender();
+		}
+	}
+	
+	@Configuration
+	@Profile("production")
+	public static class ProductionAppContext {
+		@Bean
+		public MailSender mailSender(){
+			JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+			mailSender.setHost("localhost");
+			return mailSender;
+		}
 	}
 }
