f4fda949b5bc86f64158678abc7c1c2091ffb536
hak <hak@hak-PC>
2015-06-05 22:26:54 +0900

    import 파일 정리

--- SpringTest30/src/main/java/springbook/user/dao/UserDaoJdbc.java
+++ SpringTest30/src/main/java/springbook/user/dao/UserDaoJdbc.java
@@ -3,14 +3,12 @@ package springbook.user.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import springbook.user.domain.Level;
