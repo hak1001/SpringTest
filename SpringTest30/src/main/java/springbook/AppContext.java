8232cc2a46e9d0440336ff469fbfb38682871153
hak <hak@hak-PC>
2015-06-05 22:09:54 +0900

    SqlMapConfig 인터페이스 구현하여 빈으로 등록

--- SpringTest30/src/main/java/springbook/AppContext.java
+++ SpringTest30/src/main/java/springbook/AppContext.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -17,6 +19,7 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import springbook.user.dao.UserDao;
 import springbook.user.service.DummyMailSender;
 import springbook.user.service.UserService;
 import springbook.user.service.UserServiceTest.TestUserService;
@@ -28,7 +31,7 @@ import com.mysql.jdbc.Driver;
 @ComponentScan(basePackages="springbook.user")
 @Import(SqlServiceContext.class)
 @PropertySource("/database.properties")
-public class AppContext {
+public class AppContext implements SqlMapConfig{
 	/**
 	 * @Value 필드 사용
 	 */
@@ -36,6 +39,11 @@ public class AppContext {
 	@Value("${db.url}") String url;
 	@Value("${db.username}") String username;
 	@Value("${db.password}") String password;
+	
+	@Override
+	public Resource getSqlMapResource(){
+		return new ClassPathResource("sqlmap.xml", UserDao.class);
+	}
 	
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer(){
